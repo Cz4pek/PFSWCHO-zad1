38443a1ea5dbfa67eddef5bbe48444be93a212b2
Czarek <czarek360@gmail.com>
2023-06-14 20:12:02 +0200

    fixed path for helath checks

--- demo/src/main/java/com/example/demo/controllers/BasicController.java
+++ demo/src/main/java/com/example/demo/controllers/BasicController.java
@@ -3,23 +3,26 @@ package com.example.demo.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
 import java.util.TimeZone;
 
+
 @RestController
 public class BasicController {
 
-    @GetMapping("/main")
-    public String mainPage(TimeZone timeZone, HttpServletRequest request){
+    @GetMapping("/")
+    public ResponseEntity<?> mainPage(TimeZone timeZone, HttpServletRequest request){
         // pobranie adresu IP z przeslanego zadania HTTP
         String clientIp = request.getRemoteAddr();
         // pobranie strefy czasowej, oraz uzyskanie obecnego czasu w niej
         ZonedDateTime clientLocalTime = ZonedDateTime.now(timeZone.toZoneId());
         // wyświetlenie adresu IP oraz czasu w strefie czasowej
-        return "Client ip: " + clientIp + " client local time: " + clientLocalTime.toString();
+        String res = "Client ip: " + clientIp + " client local time: " + clientLocalTime.toString();
+        return new ResponseEntity<>(res, HttpStatus.OK);    
     }
 }
